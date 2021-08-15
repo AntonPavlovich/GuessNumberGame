a69567f8f2152df6f687403d6cec1d810f7dbba7
Anton <antopavlu@gmail.com>
2021-08-15 14:38:45 +0300

    finish my game

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,109 +1,63 @@
-function myArrayProto(){
-  
-  this.myPush = function(){
-    for(let i = 0; i < arguments.length; i++) {
-      this[this.length++] = arguments[i];
-    }
-    return this.length;
+"use strict";
+
+const randomNum = () => Math.round(Math.random() * 100 + 1);
+let rightAnswer = randomNum();
+
+const prevGuessesPar = document.querySelector(".prevGuesses");
+const btn = document.querySelector(".btn");
+const aboutGuesses = document.querySelector(".aboutGuesses");
+const input = document.querySelector("#input1");
+const middleDiv = document.querySelector(".middle");
+input.focus();
+
+let counter = 1;
+let resetBtn;
+
+function submitGuess() {
+  const userGuess = Number(input.value);
+  prevGuessesPar.textContent += ` ${userGuess}`;
+  input.value = "";
+  input.focus();
+  if (userGuess > rightAnswer) {
+    aboutGuesses.textContent = "Your guess was to high";
+  } else if (userGuess < rightAnswer) {
+
+    aboutGuesses.textContent = "Your guess was to low";
   }
 
-  this.myPop = function(){
-    let delNum = this[--this.length];
-    delete this[this.length];
-    return delNum;
-  }
-  
-  this.myShift = function(){
-    let deletedNum = this[0];
-    for(i = 0;i < this.length;){
-      this[i] = this[++i]; 
-    }
-    if(this.length!==0){
-      delete this[--this.length];
-      return deletedNum;
-    }else {
-      return null;
-    }
+  if (userGuess !== rightAnswer) {
+    aboutGuesses.style.backgroundColor = "red";
+  } else if (userGuess === rightAnswer) {
+    aboutGuesses.textContent = "CONGRATULATIONS!";
+    aboutGuesses.style.backgroundColor = "green";
+    setGameOver();
   }
 
-  this.myUnShift = function(){
-    for(i = arguments.length - 1;i >= 0;i--){
-      for(j = this.length - 1;j >= 0; j--){
-        this[j + 1] = this[j];
-      }
-      this[0] = arguments[i];
-      this.length++;
-    }
-    return this.length;
+  if (counter === 10) {
+    aboutGuesses.textContent = "!!!GAME OVER!!!";
+    setGameOver();
+  }
+
+  counter++;
+}
+btn.addEventListener("click", submitGuess);
+
+function setGameOver() {
+  input.disabled = true;
+  btn.disabled = true;
+  resetBtn = document.createElement("button");
+  resetBtn.textContent = "Restart game";
+  middleDiv.append(resetBtn);
+  resetBtn.addEventListener("click", resetGame);
 }
 
-  this.myConcat = function(anotherArray){
-    let concatArr = new MyArray();
-    concatArr.length = this.length;
-    for(i=0;i<anotherArray.length;i++){
-      concatArr[i] = this[i];
-      concatArr[concatArr.length++] = anotherArray[i]; 
-    }
-    return concatArr;
-  }
-  
-//aдвансед level
-
-   this.myReverse = function(){
-    let temporaryArr = new MyArray();
-    count = 0;
-    for(j=0;j<this.length;j++){
-      temporaryArr[j] = this[j];
-    }
-    for(i=this.length-1;i>=0;i--){
-      this[count++] = temporaryArr[i];
-    }
-    return this;
-   }
-    
-   this.myReverse2 = function(){
-    let temp;
-    let x = 0;
-    let y = this.length-1;
-    while(x < y) {
-      temp = this[0];
-      this [0] = this[y];
-      this[y] = temp;
-      x++;
-      y--;
-    }
-   }
-
-   this.myForEach = function(func){ 
-      for(i=0;i<this.length;i++){
-        func(this[i],i);
-      }
-   }
-
-   this.myMap = function(func){
-      let cloneArr = new MyArray();
-      for(i=0;i<this.length;i++){
-        cloneArr[i] = func(this[i]);
-        cloneArr.length++;
-      }
-      return cloneArr;
-   }
+function resetGame() {
+  rightAnswer = randomNum();
+  counter = 1;
+  prevGuessesPar.textContent = "Your guesses: ";
+  input.disabled = false;
+  btn.disabled = false;
+  aboutGuesses.textContent = "";
+  resetBtn.remove();
+  input.focus();
 }
-
-let summ = function(elem){
-  return elem+1;
-}
-
-
-function MyArray () {
-  this.length = 0;
-  for(let i = 0; i < arguments.length; i++) {
-    this.myPush(arguments[i]);
-  }
-}
-
-
-MyArray.prototype = new myArrayProto();
-
-let arr1 = new MyArray(1,2,3);
-let arr2 = new MyArray(11,22,33,44);
